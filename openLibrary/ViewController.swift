a903ab0997ea616b9368b06478338cb85c3970ce
jdmerinor <jdmerinor@unal.edu.co>
2015-11-28 14:15:08 -0500

    Se agregan todas las funcionalidades requeridas en la entrega de la semana 2
    Entre las funcionalidades está que ya el JSON es interpretado y se
    muestra la información de los autores, el título y la portada tamaño
    mediano si la tiene.

--- openLibrary/ViewController.swift
+++ openLibrary/ViewController.swift
@@ -13,9 +13,10 @@ class ViewController: UIViewController ,UITextFieldDelegate{
     @IBOutlet weak var inputISBN: UITextField!
     @IBOutlet var textoResultado: UITextView!
     let direccionServicio = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
-        
     
-
+    @IBOutlet weak var imagenPortada: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,9 +55,10 @@ class ViewController: UIViewController ,UITextFieldDelegate{
             
             dispatch_async(dispatch_get_main_queue()) {
                 if((respuesta) != nil){
-                    self.textoResultado.text = NSString(data: datos!, encoding: NSUTF8StringEncoding)! as String
+                    self.mostrarResultadoDesdeJSON(datos,ISBN: ISBN)
+
                 }else{
-                    self.textoResultado.text = "Hubo un error al consultar el servicio"
+                    self.textoResultado.text = "Hubo un error al consultar el servicio, posiblemente no tiene acceso a internet o el servidor de OpenLibrary está presentando fallas."
                 }
             }
             
@@ -64,13 +66,98 @@ class ViewController: UIViewController ,UITextFieldDelegate{
         
         let dt = sesionConexion.dataTaskWithURL(urlAConsultar!, completionHandler: bloqueConsulta)
         dt.resume()
-        
-        
-        
-        
     }
     
     
     
+    
+    func mostrarResultadoDesdeJSON(datos : NSData?,ISBN : String){
+        do{
+            var resultadoAImprimir = ""
+            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+            let diccionarioPadre = json as! NSDictionary
+            if let nodoISBN = diccionarioPadre["ISBN:"+ISBN]{
+                let diccionarioISBN = nodoISBN as! NSDictionary
+                
+                
+                //Impresión título
+                resultadoAImprimir += "Título:\n"
+                let tituloString = diccionarioISBN["title"] as! String
+                resultadoAImprimir += tituloString + "\n"
+                
+                //Impresión autores
+                let arregloAutores = diccionarioISBN["authors"] as! NSArray
+                
+                if arregloAutores.count == 1{
+                 resultadoAImprimir += "Autor:\n"
+                }else if arregloAutores.count > 1{
+                 resultadoAImprimir += "Autores:\n"
+                }
+                for esteAutor in arregloAutores{
+                    let diccionarioAutor = esteAutor as! NSDictionary
+                    let nombreAutor = diccionarioAutor["name"]
+                    let nombreAutorString = nombreAutor as! String
+                    resultadoAImprimir += nombreAutorString + "\n"
+                }
+                
+                var direccionPortada : String? = nil
+                //Captura de la imagen portada
+                if let portada = diccionarioISBN["cover"]{
+                    let diccionarioPortada = portada as! NSDictionary
+                    if let portadaMediana = diccionarioPortada["medium"]{
+                        direccionPortada = portadaMediana as? String
+                    }
+                }
+                
+                //Muestra de los resultados
+                textoResultado.text = resultadoAImprimir
+                self.imagenPortada.image = nil
+                if direccionPortada != nil {
+                    cargarImagen(direccionPortada!)
+                }
+            }else if diccionarioPadre.allValues.count == 0{
+                self.imagenPortada.image = nil
+                self.textoResultado.text = "No existe ningún libro con el ISBN: \(ISBN)"
+            }
+            
+            
+            
+            
+        }catch let e{
+            print("Hubo un problema al parse el JSON error: \(e)" )
+        }
+    }
+    
+    
+    func cargarImagen(urlString:String)
+    {
+        let imgURL: NSURL = NSURL(string: urlString)!
+        let request: NSURLRequest = NSURLRequest(URL: imgURL)
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request){
+            (data, response, error) -> Void in
+            
+            if (error == nil && data != nil)
+            {
+                func display_image()
+                {
+                    self.imagenPortada.image = UIImage(data: data!)
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), display_image)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
 }
+
+
+
+
+
+
 
